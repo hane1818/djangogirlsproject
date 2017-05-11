77a43ac3cefb6bbcb7e077ee524263a2531e0656
Hane Liu <hane0131@gmail.com>
2017-05-11 09:33:40 +0800

    add google drive api

--- trips/models.py
+++ trips/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+import httplib2
+from apiclient import discovery
+from googleapiclient.http import MediaFileUpload
+from drive import get_credentials
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
@@ -9,3 +15,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self):
+        super().save()
+
+        credentials = get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('drive', 'v3', http=http)
+
+        file_metadata = {'name' : 'db.sqlite3'}
+        media = MediaFileUpload('db.sqlite3',
+                                mimetype='application/x-sqlite3')
+        file = service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id').execute()
+        print('File ID: %s' % file.get('id'))
